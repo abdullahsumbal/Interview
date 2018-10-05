20069ff8edcfc74fa2d2fba2af16c37e2f8c8edc
abdullahsumbal <abdullahsumbal@gmail.com>
2018-10-04 23:37:34 -0400

    hashtable added

---
+++ Data_Structures/hashtable/implementation.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+Class node {
+  int value;
+}
+
+Class hashtable{
+
+
+  public hashtable(){
+    node n = new node();
+  }
+
+  public void insert(){
+
+  }
+
+
+}
+
+Class Implementation{
+  public static void main(String [] args){
+
+  }
+}
