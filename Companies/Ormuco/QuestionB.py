68f8087d511a0116f40abbed6899d0e0b56c0e6d
abdullahsumbal <abdullahsumbal@gmail.com>
2019-01-16 12:58:35 -0500

    added template for unit test

---
+++ Companies/Ormuco/QuestionB.py
@@ -0,0 +1,16 @@
+#Question B
+
+# The goal of this question is to write a software library that accepts 2 version string as input and returns whether one is greater than, equal, or less than the other. As an example: “1.2” is greater than “1.1”. Please provide all test cases you could think of.
+import unittest
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+if __name__ == '__main__':
+    unittest.main()
