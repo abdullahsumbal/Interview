11608ea246b9ee0e740860059e35c56277b572d2
abdullahsumbal <abdullahsumbal@gmail.com>
2019-01-16 13:21:36 -0500

    added two test cases

--- Companies/Ormuco/QuestionB.py
+++ Companies/Ormuco/QuestionB.py
@@ -3,12 +3,9 @@
 # The goal of this question is to write a software library that accepts 2 version string as input and returns whether one is greater than, equal, or less than the other. As an example: “1.2” is greater than “1.1”. Please provide all test cases you could think of.
 import unittest
 
-class StringTest:
-    def __init__(self, v1, v2):
-        self.v1 = v1
-        self.v2 = v2
+class VersionChecker:
 
-    def Compare_V1_to_V2(self):
+    def compare(self):
         message = ""
         if self.v1 > self.v2:
             message = "v1 is greater than v2."
@@ -18,14 +15,36 @@ class StringTest:
             message = "v2 and v1 are equal."
 
         return message
-        
+
+    def setV2(self, v2):
+        self.v2 = v2
+
+    def setV1(self, v1):
+        self.v1 = v1
+
 class TestSum(unittest.TestCase):
 
-    def test_sum(self):
-        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+    def setUp(self):
+        """
+        Setup a version checker library
+        """
+        self.vChecker = VersionChecker()
 
-    def test_sum_tuple(self):
-        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+    def test_1(self):
+        """
+        v1 is greater than v2
+        """
+        self.vChecker.setV1("1.2")
+        self.vChecker.setV2("1.1")
+        self.assertEqual(self.vChecker.compare(), "v1 is greater than v2.", "v1 should be greater than v2")
+
+    def test_2(self):
+        """
+        v2 is greater than v1
+        """
+        self.vChecker.setV1("1.1")
+        self.vChecker.setV2("1.2")
+        self.assertEqual(self.vChecker.compare(), "v2 is greater than v1.", "v2 should be greater than v1")
 
 if __name__ == '__main__':
     unittest.main()
