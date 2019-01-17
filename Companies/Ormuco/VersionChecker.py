6b9d68633b382a4443a916e93dd0773cbc0de28a
abdullahsumbal <abdullahsumbal@gmail.com>
2019-01-16 20:45:17 -0500

    added comments for Question

--- Companies/Ormuco/VersionChecker.py
+++ Companies/Ormuco/VersionChecker.py
@@ -1,25 +1,101 @@
 import re
 
 class VersionChecker:
+    """
+    A library to compare version
+
+    ...
+
+    Attributes
+    ----------
+    v1 : str
+        first version (input)
+    v2 : str
+        second version (input)
+
+    Methods
+    -------
+    patternCheck(v1, v2)
+        Validate pattern of the verion. It should have the following pattern x.x.x
+        where x can be any number of digits
+    typeCheck(v)
+        Validate type. the version input should be a string
+    compare()
+        Return the comparsion information for two version.
+    setV1(v1)
+        Set version one value
+    setV2(v1)
+        Set version two value
+    """
 
     def __init__(self, v1=None, v2=None):
+        """
+        Parameters
+        ----------
+        v1 : str
+            first version (input)
+        v2 : str
+            second version (input)
+        """
         self.v1 = v1
         self.v2 = v2
 
     def patternCheck(self, v):
+        """
+        Validate pattern of the verion. It should have the following pattern x.x.x
+        where x can be any number of digits
+
+        Parameters
+        ----------
+        v: str
+            version
+
+        Returns
+        -------
+        corretPattern: Boolean
+            True if the pattern is correct otherwise False
+        """
+        corretPattern = True
         m = re.match('^\d+\.\d+\.\d+$|^\d+\.\d+$', v)
         if m == None:
-            return False
-        return True
+            corretPattern = False
+        return corretPattern
 
     def typeCheck(self, v):
+        """
+        Validate if the version is inputed in the string type
+
+        Parameters
+        ----------
+        v: str
+            version
+
+        Returns
+        -------
+        isString: Boolean
+            Return True if the version is string otherwise False
+        """
+        isString = True
         if not isinstance(v, str):
-            return False
-        return True
+            isString = False
+        return isString
 
 
     def compare(self):
+        """Return comparsion information.
+        Parameters
+        ----------
 
+        Returns
+        -------
+        message: string
+            comparsion result
+
+        Raises
+        ------
+        Excepton
+            If both version are not set
+        """
         # check if both version are set
         if self.v1 == None:
             raise Exception('Please set version 1 value.')
@@ -38,6 +114,17 @@ class VersionChecker:
         return message
 
     def setV2(self, v2):
+        """Set version 2 input value
+        Parameters
+        ----------
+        v2: str
+            version 2 input value
+
+        Raises
+        ------
+        Excepton
+            if version is not string or has an incorrect pattern
+        """
         if not self.typeCheck(v2):
             raise Exception('v2 should be a string')
         if not self.patternCheck(v2):
@@ -45,6 +132,17 @@ class VersionChecker:
         self.v2 = v2
 
     def setV1(self, v1):
+        """Set version 1 input value
+        Parameters
+        ----------
+        v2: str
+            version 1 input value
+
+        Raises
+        ------
+        Excepton
+            if version is not string or has an incorrect pattern
+        """
         if not self.typeCheck(v1):
             raise Exception('v1 should be a string')
         if not self.patternCheck(v1):
